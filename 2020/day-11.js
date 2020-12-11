99ef8daa7e96838fd410b43852f526fe5b2a755c
Jakob <jakobchis@gmail.com>
2020-12-11 18:58:36 -0500

    completed day 11 part 2

--- 2020/day-11.js
+++ 2020/day-11.js
@@ -1,4 +1,4 @@
-const { Console } = require("console");
+const { Console, countReset } = require("console");
 const fs = require("fs");
 
 const readFile = () => {
@@ -19,16 +19,91 @@ const readFile = () => {
 
 const getOccupiedAdjacentSeats = (seats, y, x) => {
     let adjacentSeats = 0;
-    if (seats[y - 1] && seats[y - 1][x - 1] && seats[y - 1][x - 1] === "#") adjacentSeats += 1;
-    if (seats[y - 1] && seats[y - 1][x] && seats[y - 1][x] === "#") adjacentSeats += 1;
-    if (seats[y - 1] && seats[y - 1][x + 1] && seats[y - 1][x + 1] === "#") adjacentSeats += 1;
-    if (seats[y][x - 1] && seats[y][x - 1] === "#") adjacentSeats += 1;
-    if (seats[y][x + 1] && seats[y][x + 1] === "#") adjacentSeats += 1;
-    if (seats[y + 1] && seats[y + 1][x - 1] && seats[y + 1][x - 1] === "#") adjacentSeats += 1;
-    if (seats[y + 1] && seats[y + 1][x] && seats[y + 1][x] === "#") adjacentSeats += 1;
-    if (seats[y + 1] && seats[y + 1][x + 1] && seats[y + 1][x + 1] === "#") adjacentSeats += 1;
+    // dict of directions and their +y and +x operations
+    let directions = {
+        nw: [-1, -1],
+        n: [-1, 0],
+        ne: [-1, 1],
+        w: [0, -1],
+        e: [0, 1],
+        sw: [1, -1],
+        s: [1, 0],
+        se: [1, 1],
+    };
+
+    for (const dir in directions) {
+        const yNew = y + directions[dir][0];
+        const xNew = x + directions[dir][1];
+        if (seats[yNew] && seats[yNew][xNew] && seats[yNew][xNew] === "#") adjacentSeats += 1;
+    }
 
     return adjacentSeats;
+};
+
+const getOccupiedVisibleSeats = (seats, y, x) => {
+    let visibleSeatsCount = 0;
+    let visibleSeats = {};
+    // dict of directions and their +y and +x operations
+    let directions = {
+        nw: [-1, -1],
+        n: [-1, 0],
+        ne: [-1, 1],
+        w: [0, -1],
+        e: [0, 1],
+        sw: [1, -1],
+        s: [1, 0],
+        se: [1, 1],
+    };
+
+    for (const dir in directions) {
+        let running = true;
+        let yInc = 0;
+        let xInc = 0;
+
+        while (running) {
+            if (yInc === seats.length - 1 && xInc === seats[0].length - 1) {
+                running = false;
+                break;
+            }
+
+            let yNew = y;
+            if (directions[dir][0] !== 0) {
+                yNew = y + directions[dir][0] + (directions[dir][0] < 0 ? -yInc : yInc);
+            }
+            let xNew = x;
+            if (directions[dir][1] !== 0) {
+                xNew = x + directions[dir][1] + (directions[dir][1] < 0 ? -xInc : xInc);
+            }
+
+            if (!seats[yNew] || !seats[yNew][xNew]) {
+                running = false;
+                break;
+            }
+
+            if (!visibleSeats.hasOwnProperty(dir)) {
+                if (seats[yNew][xNew] === "#") {
+                    visibleSeats[dir] = "#";
+                } else if (seats[yNew][xNew] === "L") {
+                    visibleSeats[dir] = "L";
+                }
+            }
+
+            if (yInc < seats.length - 1) {
+                yInc += 1;
+            }
+            if (xInc < seats[0].length - 1) {
+                xInc += 1;
+            }
+        }
+    }
+
+    for (const dir in visibleSeats) {
+        if (visibleSeats[dir] === "#") {
+            visibleSeatsCount += 1;
+        }
+    }
+
+    return visibleSeatsCount;
 };
 
 const part1 = () => {
@@ -59,6 +134,7 @@ const part1 = () => {
                 }
             }
         }
+
         if (originalOccupiedSeats === occupiedSeats) {
             running = false;
         }
@@ -67,4 +143,42 @@ const part1 = () => {
     console.log("Part 1 occupied seats: " + occupiedSeats);
 };
 
+const part2 = () => {
+    let seats = readFile();
+    let occupiedSeats = 0;
+    let running = true;
+
+    while (running) {
+        let originalSeats = [];
+        for (const row of seats) {
+            originalSeats.push(row.slice());
+        }
+        let originalOccupiedSeats = occupiedSeats;
+
+        for (let i = 0; i < originalSeats.length; i++) {
+            for (let n = 0; n < originalSeats[i].length; n++) {
+                const visibleSeats = getOccupiedVisibleSeats(originalSeats, i, n);
+                if (originalSeats[i][n] === "L") {
+                    if (visibleSeats === 0) {
+                        seats[i][n] = "#";
+                        occupiedSeats += 1;
+                    }
+                } else if (originalSeats[i][n] === "#") {
+                    if (visibleSeats >= 5) {
+                        seats[i][n] = "L";
+                        occupiedSeats -= 1;
+                    }
+                }
+            }
+        }
+
+        if (originalOccupiedSeats === occupiedSeats) {
+            running = false;
+        }
+    }
+
+    console.log("Part 2 occupied seats: " + occupiedSeats);
+};
+
 part1();
+part2();
