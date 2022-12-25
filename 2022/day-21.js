e39c313dc13dd5dbf665a321de84490cf162cb2c
Jakob Chisholm <jakobchis@gmail.com>
2022-12-25 16:40:42 -0500

    completed dec 21 part 2

--- 2022/day-21.js
+++ 2022/day-21.js
@@ -1,5 +1,4 @@
 import { readFileSync } from "fs";
-import lodash from "lodash";
 
 const testData = readFileSync("day-21.txt", "utf8")
   .split("\n")
@@ -16,27 +15,49 @@ const testData = readFileSync("day-21.txt", "utf8")
     };
   }, {});
 
-const recurseThroughMonkeys = (monkey) => {
-  if (monkey.operation === undefined) {
-    return Number(monkey.firstNumber);
-  } else {
-    const firstNumber = recurseThroughMonkeys(testData[monkey.firstNumber]);
-    const secondNumber = recurseThroughMonkeys(testData[monkey.secondNumber]);
-
-    if (monkey.operation === "+") {
-      return firstNumber + secondNumber;
-    } else if (monkey.operation === "*") {
-      return firstNumber * secondNumber;
-    } else if (monkey.operation === "-") {
-      return firstNumber - secondNumber;
-    } else if (monkey.operation === "/") {
-      return firstNumber / secondNumber;
-    }
-  }
-};
-
 const part1 = () => {
+  const recurseThroughMonkeys = (monkey) => {
+    if (monkey.operation === undefined) {
+      return Number(monkey.firstNumber);
+    } else {
+      const firstNumber = recurseThroughMonkeys(testData[monkey.firstNumber]);
+      const secondNumber = recurseThroughMonkeys(testData[monkey.secondNumber]);
+
+      if (monkey.operation === "+") {
+        return firstNumber + secondNumber;
+      } else if (monkey.operation === "*") {
+        return firstNumber * secondNumber;
+      } else if (monkey.operation === "-") {
+        return firstNumber - secondNumber;
+      } else if (monkey.operation === "/") {
+        return firstNumber / secondNumber;
+      }
+    }
+  };
+
   return recurseThroughMonkeys(testData["root"]);
 };
 
+const part2 = () => {
+  const recurseThroughMonkeys = (monkeyName) => {
+    const monkeyData = testData[monkeyName];
+
+    if (monkeyData.operation === undefined) {
+      return monkeyName === "humn" ? "x" : monkeyData.firstNumber;
+    } else {
+      const firstNumber = recurseThroughMonkeys(monkeyData.firstNumber);
+      const secondNumber = recurseThroughMonkeys(monkeyData.secondNumber);
+
+      return `(${firstNumber}${monkeyData.operation}${secondNumber})`;
+    }
+  };
+
+  // meme solve, run this equation through MathPapa and then round down to nearest integer
+  const equation = `${recurseThroughMonkeys(
+    testData["root"].firstNumber
+  )}=${recurseThroughMonkeys(testData["root"].secondNumber)}`;
+  return equation;
+};
+
 console.log(part1());
+console.log(part2());
