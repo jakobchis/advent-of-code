49ae535e455eb2111ecf5d0444e10e99b8dbd23a
Jakob Chisholm <jakobchis@gmail.com>
2023-11-29 21:32:16 -0500

    Set up C# 2023 project, restructure files

---
+++ AOC2023/AOC2023/Days/Day01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2023.Days
+{
+    internal class Day01
+    {
+        public void Play()
+        {
+            var contents = File.ReadAllText(
+                "C:\\Users\\jakob\\Documents\\GitHub\\advent-of-code\\AOC2023\\AOC2023\\Days\\Day01.txt"
+            );
+            var splitContents = contents.Split("\r\n\r\n");
+        }
+    }
+}
---
+++ AOC2023/AOC2023/Program.cs
@@ -0,0 +1,4 @@
+﻿using AOC2023.Days;
+
+var Day01 = new Day01();
+Day01.Play();
