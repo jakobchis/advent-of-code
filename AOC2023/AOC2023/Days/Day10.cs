a601cc054a4c2c3957a1afa0ecfff723dd7e3b5c
Jakob Chisholm <jakobchis@gmail.com>
2023-12-10 18:45:53 -0500

    Day 10 part 1 2023

---
+++ AOC2023/AOC2023/Days/Day10.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+namespace AOC2023.Days
+{
+    internal class Day10
+    {
+        static string input = File.ReadAllText(
+            "C:\\Users\\jakob\\Documents\\GitHub\\advent-of-code\\AOC2023\\AOC2023\\Days\\Day10.txt"
+        );
+
+        public class Node
+        {
+            public string Symbol = "";
+            public int Y;
+            public int X;
+        }
+
+        public static void Part1()
+        {
+            var nodeGrid = input
+                .Split("\r\n")
+                .Select(
+                    (row, rowIndex) =>
+                        row.ToArray()
+                            .Select(
+                                (col, colIndex) =>
+                                    new Node
+                                    {
+                                        Y = rowIndex,
+                                        X = colIndex,
+                                        Symbol = col.ToString()
+                                    }
+                            )
+                            .ToList()
+                )
+                .ToList();
+            var filledNodes = new List<Node>();
+            var startingNode = new Node();
+
+            foreach (var nodeRow in nodeGrid)
+            {
+                foreach (var nodeCol in nodeRow)
+                {
+                    if (nodeCol.Symbol == "S")
+                    {
+                        startingNode.X = nodeCol.X;
+                        startingNode.Y = nodeCol.Y;
+                        startingNode.Symbol = "7"; // hardcoded by looking at input
+                    }
+                }
+            }
+
+            var queue = new Queue<Node>();
+            queue.Enqueue(startingNode);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (filledNodes.Find(n => n.Y == node.Y && n.X == node.X) != null)
+                {
+                    continue;
+                }
+
+                filledNodes.Add(node);
+
+                switch (node.Symbol)
+                {
+                    case "|":
+                        queue.Enqueue(nodeGrid[node.Y - 1][node.X]);
+                        queue.Enqueue(nodeGrid[node.Y + 1][node.X]);
+                        break;
+                    case "-":
+                        queue.Enqueue(nodeGrid[node.Y][node.X - 1]);
+                        queue.Enqueue(nodeGrid[node.Y][node.X + 1]);
+                        break;
+                    case "L":
+                        queue.Enqueue(nodeGrid[node.Y - 1][node.X]);
+                        queue.Enqueue(nodeGrid[node.Y][node.X + 1]);
+                        break;
+                    case "J":
+                        queue.Enqueue(nodeGrid[node.Y - 1][node.X]);
+                        queue.Enqueue(nodeGrid[node.Y][node.X - 1]);
+                        break;
+                    case "7":
+                        queue.Enqueue(nodeGrid[node.Y + 1][node.X]);
+                        queue.Enqueue(nodeGrid[node.Y][node.X - 1]);
+                        break;
+                    case "F":
+                        queue.Enqueue(nodeGrid[node.Y + 1][node.X]);
+                        queue.Enqueue(nodeGrid[node.Y][node.X + 1]);
+                        break;
+                    case ".":
+                        break;
+                    case "S":
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Part 1: {filledNodes.Count / 2}");
+        }
+
+        public static void Part2() { }
+    }
+}
--- AOC2023/AOC2023/Program.cs
+++ AOC2023/AOC2023/Program.cs
@@ -16,5 +16,7 @@
 //Day07.Part2();
 //Day08.Part1();
 //Day08.Part2();
-Day09.Part1();
-Day09.Part2();
+//Day09.Part1();
+//Day09.Part2();
+Day10.Part1();
+Day10.Part2();
