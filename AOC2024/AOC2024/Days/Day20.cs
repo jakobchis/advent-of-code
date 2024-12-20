a3a9003d48a593bf0fb1345f43cf10dd141df235
Jakob Chisholm <jakobchis@gmail.com>
2024-12-20 12:03:02 -0500

    Cleanup

--- AOC2024/AOC2024/Days/Day20.cs
+++ AOC2024/AOC2024/Days/Day20.cs
@@ -12,56 +12,16 @@ public class Day20
     // PART 1
     public void Part01()
     {
-        var grid = input.Split("\n").Select(row => row.ToCharArray().ToList()).ToList();
-
-        var startY = grid.FindIndex(row => row.Contains('S'));
-        var startX = grid[startY].IndexOf('S');
-        var startPos = new Vector2(startX, startY);
-        var endY = grid.FindIndex(row => row.Contains('E'));
-        var endX = grid[endY].IndexOf('E');
-        var endPos = new Vector2(endX, endY);
-
-        List<Vector2> directions = [new(-1, 0), new(0, 1), new(1, 0), new(0, -1)];
-
-        var (_, visited) = Dijkstra(grid, startPos, endPos, directions);
-
-        Dictionary<int, int> timeSavedCounts = new();
-        foreach (var node in visited)
-        {
-            var laterVisitedNodes = visited.Where(v =>
-            {
-                var isDifferentNode = v != node;
-                var isWithinBounds = Math.Abs(v.X - node.X) <= 2 && Math.Abs(v.Y - node.Y) <= 2;
-
-                return isDifferentNode && isWithinBounds;
-            });
-
-            foreach (var laterVisitedNode in laterVisitedNodes)
-            {
-                var (shortestPath, _) = Dijkstra(grid, node, laterVisitedNode, directions, false);
-
-                if (shortestPath <= 2)
-                {
-                    var (timeSaved, _) = Dijkstra(grid, node, laterVisitedNode, directions);
-                    timeSaved -= 2; // -2 for the time the cheat takes
-                    if (timeSaved > 0)
-                    {
-                        timeSavedCounts.TryGetValue(timeSaved, out int time);
-                        timeSavedCounts[timeSaved] = time + 1;
-                    }
-                }
-            }
-        }
-
-        timeSavedCounts = timeSavedCounts.OrderBy(cts => cts.Key).ToDictionary();
-        // TODO figure out why counts are doubled up
-        var answer = timeSavedCounts.Where(cts => cts.Key >= 100).Sum(cts => cts.Value / 2);
-
-        Console.WriteLine($"Part 1: {answer}");
+        Console.WriteLine($"Part 1: {GetAnswer(2)}");
     }
 
     // PART 2
     public void Part02()
+    {
+        Console.WriteLine($"Part 2: {GetAnswer(20)}");
+    }
+
+    private int GetAnswer(int cheatSize)
     {
         var grid = input.Split("\n").Select(row => row.ToCharArray().ToList()).ToList();
 
@@ -74,67 +34,51 @@ public class Day20
 
         List<Vector2> directions = [new(-1, 0), new(0, 1), new(1, 0), new(0, -1)];
 
-        var (_, visited) = Dijkstra(grid, startPos, endPos, directions);
+        var (_, visited) = Dijkstra(grid, startPos, endPos, directions, collision: true);
 
         Dictionary<int, int> timeSavedCounts = new();
-        var visitedList = new Queue<Vector2>();
+        var visitedQueue = new Queue<Vector2>();
         foreach (var v in visited)
         {
-            visitedList.Enqueue(v);
+            visitedQueue.Enqueue(v);
         }
 
-        while (visitedList.Count > 0)
+        while (visitedQueue.Count > 0)
         {
-            var visitedNode = visitedList.Dequeue();
-            Console.WriteLine($"Checking node {visitedNode}");
-            // var laterVisitedNodes = visitedList
-            //     .GetRange(i + 1, visitedList.Count - i - 1)
-            //     .Where(v =>
-            //     {
-            //         var isDifferentNode = v != node;
-            //         var isWithinBounds =
-            //             Math.Abs(v.X - node.X) <= 20 && Math.Abs(v.Y - node.Y) <= 20;
-            //
-            //         return isDifferentNode && isWithinBounds;
-            //     });
+            var visitedNode = visitedQueue.Dequeue();
 
-            var laterVisitedNodes = visitedList;
-
-            foreach (var laterVisitedNode in laterVisitedNodes)
+            foreach (var laterVisitedNode in visitedQueue)
             {
-                var isWithinBounds = Vector2.Distance(visitedNode, laterVisitedNode) <= 20;
+                var isWithinBounds = Vector2.Distance(visitedNode, laterVisitedNode) <= cheatSize;
                 if (!isWithinBounds)
                 {
                     continue;
                 }
 
-                var (shortestPath, _) = Dijkstra(
+                var (shortestPathWithoutCollision, _) = Dijkstra(
                     grid,
                     visitedNode,
                     laterVisitedNode,
                     directions,
-                    false
+                    collision: false
                 );
 
-                if (shortestPath <= 20)
+                if (shortestPathWithoutCollision <= cheatSize)
                 {
-                    var (timeSaved, _) = Dijkstra(grid, visitedNode, laterVisitedNode, directions);
-                    timeSaved -= shortestPath; // -2 for the time the cheat takes
+                    var (shortestPathWithCollision, _) = Dijkstra(grid, visitedNode, laterVisitedNode, directions, collision: true);
+                    var timeSaved = shortestPathWithCollision - shortestPathWithoutCollision;
                     if (timeSaved > 0)
                     {
-                        timeSavedCounts.TryGetValue(timeSaved, out int time);
-                        timeSavedCounts[timeSaved] = time + 1;
+                        timeSavedCounts.TryGetValue(timeSaved, out int count);
+                        timeSavedCounts[timeSaved] = count + 1;
                     }
                 }
             }
         }
 
-        timeSavedCounts = timeSavedCounts.OrderBy(cts => cts.Key).ToDictionary();
         var answer = timeSavedCounts.Where(cts => cts.Key >= 100).Sum(cts => cts.Value);
 
-        // TODO make it faster, memoization?
-        // 1004388 too low
-        Console.WriteLine($"Part 2: {answer}");
+        return answer;
     }
 
     private bool InBounds(Vector2 position, List<List<char>> grid)
@@ -150,7 +94,7 @@ public class Day20
         Vector2 startPos,
         Vector2 endPos,
         List<Vector2> directions,
-        bool collision = true
+        bool collision
     )
     {
         var costs = new Dictionary<Vector2, int> { [startPos] = 0 };
