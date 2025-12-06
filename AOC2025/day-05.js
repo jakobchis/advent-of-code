3e9c6f342837bd1eef0a63cad888888a423b82c0
Jakob Chisholm <jakobchis@gmail.com>
2025-12-06 09:07:03 -0500

    Day 05 Part 2 2025

--- AOC2025/day-05.js
+++ AOC2025/day-05.js
@@ -28,48 +28,37 @@ const part2 = () => {
   let freshRanges = input
     .split("\n\n")[0]
     .split("\n")
-    .map((range) => range.split("-").map(Number));
-  let numFreshIngredients = 0;
+    .map((range) => range.split("-").map(Number))
+    .sort((a, b) => a[0] - b[0]);
 
-  // TODO sort and then merge overlapping ranges instead
-  // sort by the first number in the range, last number tiebreaker
-  // iterate over ranges, keep a list of merged ranges
-  // if mergedRanges is empty, or the current range's start is after the last merged range's end, add it
-  // otherwise, merge the current range into the last range (extend the end)
+  let numFreshIngredients = 0;
+  let mergedRanges = [];
 
   for (let i = 0; i < freshRanges.length; i++) {
-    const [start, end] = freshRanges[i];
-    numFreshIngredients += end - start + 1;
+    const currentRange = freshRanges[i];
+    const lastMergedRange = mergedRanges[mergedRanges.length - 1];
 
-    if (i > 0) {
-      for (let n = 0; n < i; n++) {
-        const intersection = intersectTwoRanges(freshRanges[i], freshRanges[n]);
-        if (!intersection) {
-          continue;
-        }
-
-        const [prevStart, prevEnd] = intersection;
-        numFreshIngredients -= prevEnd - prevStart + 1;
-      }
+    // Add first range
+    if (i === 0) {
+      mergedRanges.push(currentRange);
+      continue;
     }
+
+    // Check if current range's start is after last merged range's end
+    if (currentRange[0] > lastMergedRange[1]) {
+      mergedRanges.push(currentRange);
+      continue;
+    }
+
+    // Otherwise, merge
+    lastMergedRange[1] = Math.max(lastMergedRange[1], currentRange[1]);
+  }
+
+  for (let i = 0; i < mergedRanges.length; i++) {
+    numFreshIngredients += mergedRanges[i][1] - mergedRanges[i][0] + 1;
   }
 
   return numFreshIngredients;
-};
-
-const intersectTwoRanges = (rangeA, rangeB) => {
-  const [startA, endA] = rangeA;
-  const [startB, endB] = rangeB;
-
-  const intersectionStart = Math.max(startA, startB);
-
-  const intersectionEnd = Math.min(endA, endB);
-
-  if (intersectionStart <= intersectionEnd) {
-    return [intersectionStart, intersectionEnd];
-  } else {
-    return null;
-  }
 };
 
 console.log(part1());
